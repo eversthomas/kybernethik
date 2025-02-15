d5b75686d421ff1a6b2c183b18f6e2b4ef23b815
Thomas <evers.thomas@gmail.com>
2025-02-15 09:37:31 +0100

    Update home.php

--- home.php
+++ home.php
@@ -19,16 +19,16 @@
 				<?php } ?>
 			
 			<!-- Titel mit Permalink -->
-			<h2><a href="<?= get_permalink(); ?>"><?= get_the_title(); ?></a></h2>
+			<h2><a href="<?php get_permalink(); ?>"><?php get_the_title(); ?></a></h2>
 
 			<!-- Metadaten: Erstellungsdatum & Autor -->
-			<p>Veröffentlicht am <?= get_the_date(); ?> von <?= get_the_author(); ?></p>
+			<p>Veröffentlicht am <?php get_the_date(); ?> von <?php get_the_author(); ?></p>
 
 			<!-- Excerpt (Kurzbeschreibung) -->
-			<p><?= get_the_excerpt(); ?></p>
+			<p><?php get_the_excerpt(); ?></p>
 
 			<!-- "Weiterlesen" Link -->
-			<p><a href="<?= get_permalink(); ?>"><?=__('Weiterlesen »', 'textdomain'); ?></a></p>
+			<p><a href="<?php get_permalink(); ?>"><?=__('Weiterlesen »', 'textdomain'); ?></a></p>
 
 		</article>
     <?php endwhile; ?>
@@ -44,6 +44,6 @@
 
 </main>
 
-<?= get_sidebar(); ?>
+<?php get_sidebar(); ?>
 
-<?= get_footer(); ?>
+<?php get_footer(); ?>
