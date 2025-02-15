92c6352be63ef47d2e61070958c6192118741ae5
Thomas <evers.thomas@gmail.com>
2025-02-15 09:39:00 +0100

    Update single.php

--- single.php
+++ single.php
@@ -12,18 +12,18 @@
 
 		<!-- Beitragsbild (falls vorhanden) -->
 		<?php if (has_post_thumbnail()) : ?>
-			<a href="<?= get_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+			<a href="<?php get_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
 		<?php endif; ?>
 
 
-		<h2><?= get_the_title(); ?></h2>
+		<h2><?php get_the_title(); ?></h2>
 
     	<!-- Metadaten: Erstellungsdatum & Autor -->
-		<p>Veröffentlicht am <?= get_the_date(); ?> von <?= get_the_author(); ?></p>
+		<p>Veröffentlicht am <?php get_the_date(); ?> von <?php get_the_author(); ?></p>
 
     	<!-- Excerpt (Kurzbeschreibung) -->
-		<p><?= get_the_excerpt(); ?></p>
-    	<?= the_content(); ?>
+		<p><?php get_the_excerpt(); ?></p>
+    	<?php the_content(); ?>
     
 		<!-- Navigation zu vorherigem und nächstem Beitrag -->
     	<?php
@@ -36,6 +36,6 @@
 	</article>
 </main>
 
-<?= get_sidebar(); ?>
+<?php get_sidebar(); ?>
 
-<?= get_footer(); ?>
+<?php get_footer(); ?>
